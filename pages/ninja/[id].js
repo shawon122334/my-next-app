4c3a62789f1bfba609a919eda18490eaa16eba2a
shawon122334 <shawon.mu11@gmail.com>
2021-05-09 15:14:30 +0600

    dynamic routes done

--- pages/ninja/[id].js
+++ pages/ninja/[id].js
@@ -1,7 +1,33 @@
-const Details = () => {
+export const getStaticPaths= async()=>{
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json()
+
+    const paths= data.map(user=>{
+        return{
+            params:{id:user.id.toString()}
+        }
+    })
+    return{
+        paths,
+        fallback:false
+    }
+}
+export const getStaticProps= async (context) =>{
+    const id = context.params.id;
+    const res=await fetch('https://jsonplaceholder.typicode.com/users/'+id)
+    const data=await res.json();
+
+    return{
+        props:{user1:data}
+    }
+}
+const Details = ({user1}) => {
     return ( 
         <div>
-            <h3>this is details</h3>
+            <h1>{user1.name}</h1>
+            <p>{user1.email}</p>
+            <p>{user1.website}</p>
+            <p>{user1.address.city}</p>
         </div>
      );
 }
