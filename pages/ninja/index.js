5507c5a3b76020012047d2e5babd00a4ead84a5b
shawon122334 <shawon.mu11@gmail.com>
2021-05-09 09:25:50 +0600

    data fetched

--- pages/ninja/index.js
+++ pages/ninja/index.js
@@ -1,11 +1,27 @@
+import styles from '../../styles/users.module.css';
 import Head from 'next/head'
-const Ninja = () => {
+export const getStaticProps= async () =>{
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data= await res.json();
+
+    return{
+        props:{users:data}
+    }
+}
+const Ninja = ({users}) => {
     return (
         <>
             <Head>
                 <title>Next App | Ninja</title>
             </Head>
-            <div>this is root path for ninja </div>
+            <div>
+                <h1>All Users</h1>
+                {users.map(user=>(
+                    <div key={user.id}>
+                        <a className={styles.single}><h3>{user.name}</h3></a>
+                    </div>
+                ))}
+            </div>
         </>
     );
 }
