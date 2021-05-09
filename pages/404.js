f937b395f0c64549d25619d948dd538a0e032083
shawon122334 <shawon.mu11@gmail.com>
2021-05-09 07:58:13 +0600

    url not found redirect to homepage

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,14 @@
 import Link from 'next/link'
+import { useEffect } from 'react';
+import {useRouter} from 'next/router'
 const NotFound = () => {
+    const router=useRouter();
+    useEffect(() => {
+        setTimeout(() =>{
+            // router.go(-1)
+            router.push('/')
+        },3000)
+    }, []);
     return ( 
         <div className="notFound">
             <h1>Opssss...</h1>
