599015c6216bee2c7ab2a243aaff5555e3d90539
shawon122334 <shawon.mu11@gmail.com>
2021-05-09 07:34:44 +0600

    style added

--- comps/Footer.js
+++ comps/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
-        <div>
+        <footer>
             <p>Copyright 2021 sabbir</p>
-        </div>
+        </footer>
     );
 }
  
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,13 @@ import Image from 'next/image'
 import Footer from '../comps/Footer'
 import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div >
-      <h1> home page</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo iste numquam repellendus ipsa unde commodi quia earum dignissimos incidunt maxime. Sed accusamus quo, eum a accusantium non voluptatem harum eaque?
+      <h1 className={styles.title}> home page</h1>
+      <p className={styles.text}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo iste numquam repellendus ipsa unde commodi quia earum dignissimos incidunt maxime. Sed accusamus quo, eum a accusantium non voluptatem harum eaque?
       Iusto ipsa voluptate et animi nam! Accusantium odit, harum voluptatibus porro vel eum quibusdam unde doloribus, impedit ex rem aliquam. Doloribus, quibusdam tempora beatae quam non iure expedita tenetur ipsum!
       Amet rem sequi nostrum id officia velit nihil eum, magnam assumenda, deleniti vitae hic at, alias facilis a adipisci placeat odit esse. Maxime voluptatem consectetur reprehenderit assumenda omnis architecto deleniti.
       Provident, earum eveniet modi rerum necessitatibus voluptatum delectus reiciendis tenetur facilis dolorum esse, non consectetur, saepe sed recusandae nam. Exercitationem cum provident dolorum impedit excepturi harum, vitae reiciendis reprehenderit autem.
@@ -18,6 +19,7 @@ export default function Home() {
       Autem quae harum consequatur, aliquid facilis incidunt animi architecto culpa. Illo laboriosam necessitatibus ipsum rerum quisquam aliquam quasi omnis dolore voluptatem labore quidem, quos est, atque reprehenderit quae consequatur dignissimos.
       Esse consequuntur voluptatem, quae amet quaerat hic pariatur sequi iste optio, praesentium quasi beatae, corporis natus sed magni architecto. Neque aliquid dolor sit corporis tenetur ut voluptates ratione tempore! Obcaecati.
       Doloremque consequatur hic itaque distinctio deleniti recusandae libero eligendi molestiae assumenda, aliquid id iste maiores, ipsam minus laboriosam excepturi ab nemo cum nulla ad? Architecto omnis veniam consequatur cum a?</p>
+      <Link href="/ninja" ><a className={styles.btn}>Visit Nija page</a></Link>
     </div>
   )
 }
